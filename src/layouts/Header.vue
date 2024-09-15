529b25041633962273cce6208c07a9bdf24e8403
Twilight <im@ivampiresp.com>
2024-09-15 23:17:57 +0800

    改进 手机显示

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -137,7 +137,7 @@ const isMobile = useIsMobile();
 const showDrawer = ref(false);
 const width = computed(() => {
   if (isMobile.value) {
-    // return window.innerWidth;
+    return window.innerWidth;
   } else {
     return window.innerWidth * 0.4;
   }
