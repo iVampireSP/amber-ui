3bf02d46ce2158dd53586ec38c2b074e4b60460c
Twilight <im@ivampiresp.com>
2024-09-15 21:38:05 +0800

    改进

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -137,9 +137,9 @@ const isMobile = useIsMobile();
 const showDrawer = ref(false);
 const width = computed(() => {
   if (isMobile.value) {
-    return window.innerWidth;
+    // return window.innerWidth;
   } else {
-    return window.innerWidth * 0.5;
+    return window.innerWidth * 0.4;
   }
 })
 
