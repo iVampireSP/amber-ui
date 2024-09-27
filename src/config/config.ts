cce7595932a892b067b0b4797089497ee1b44f87
Twilight <im@ivampiresp.com>
2024-09-27 14:20:20 +0800

    移除 多余的导入

--- src/config/config.ts
+++ src/config/config.ts
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
   config.oauth_client_id = "16";
 }
 
-// config.backend = "https://amber-api.leaflow.cn";
+config.backend = "https://amber-api.leaflow.cn";
 
 // console.log("api endpoint: " + config.backend);
 
